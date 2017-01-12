3019b9c6a1801867f71448d0acc99441e83cab19
Robert Lodahl <robert.lodahl@med.uni-goettingen.de>
2017-01-12 14:12:53 +0100

    Added a modified BuildExtraLowDimDf method to the phenotype heatmap, summarizing all needed data in one data.frame

--- web-app/HeimScripts/phenotypeHeatmap/run.R
+++ web-app/HeimScripts/phenotypeHeatmap/run.R
@@ -52,6 +52,77 @@ parseInputLd<- function() {
   return(ld)
 }
 
+buildLowDim <- function(ld.list) {
+  if(is.null(ld.list)){
+    return(NULL)
+  }
+  
+  ld.names <- unlist(names(ld.list))
+  ld.namesWOSubset <- sub("_s[1-2]{1}$", "", ld.names)
+  ld.fullNames <- sapply(ld.namesWOSubset, function(el) fetch_params$ontologyTerms[[el]]$fullName)
+  ld.fullNames <- as.character(as.vector(ld.fullNames))
+  split <- strsplit2(ld.fullNames, "\\\\")
+  ld.rownames <- apply(split, 1, function(row) paste(tail(row[row != ""], n=2), collapse="//"))
+  ##ld.subsets <- as.integer(sub("^.*_s", "", ld.names))
+  ld.types <- sub("_.*$", "", ld.names)
+
+  PARENT.vec = character(length = 0)
+  ROWNAME.vec = character(length = 0)
+  PATIENTID.vec = character(length = 0)  
+  VALUE.vec = character(length = 0)
+  ##COLNAME.vec = character(length = 0)
+  TYPE.vec = character(length = 0)
+  ##SUBSET.vec = character(length = 0)
+  ZSCORE.vec = character(length = 0)
+
+  for (i in 1:length(ld.names)) {
+      ld.var <- ld.list[[i]]
+      for (j in 1:nrow(ld.var)) {
+          ld.patientID <- ld.var[j, 1]
+          ld.value <- ld.var[j, 2]
+          if (ld.value == "" || is.na(ld.value)) next
+          ld.type <- ld.types[i]
+          ##ld.subset <- ld.subsets[i]
+          ld.rowname.tmp <- ld.rownames[i]
+          ##ld.colname <- paste(ld.patientID, ld.rowname.tmp, paste("s", ld.subset, sep=""), sep="_")
+          ld.rowname <- ld.rowname.tmp
+          ROWNAME.vec <- c(ROWNAME.vec, ld.rowname)
+          tmpParentPos <- regexpr("//", ld.rowname)[1]
+          PARENT.vec <- c(PARENT.vec, substring(ld.rowname, 1, tmpParentPos-1))
+          PATIENTID.vec <- c(PATIENTID.vec, ld.patientID)
+          VALUE.vec <- c(VALUE.vec, ld.value)
+          ##COLNAME.vec <- c(COLNAME.vec, ld.colname)
+          TYPE.vec <- c(TYPE.vec, ld.type)
+          ##SUBSET.vec <- c(SUBSET.vec, ld.subset)
+      }
+  }
+
+
+  res.df = data.frame(PATIENTID = as.integer(PATIENTID.vec),
+                      ##COLNAME = COLNAME.vec,
+                      PARENT = PARENT.vec,
+                      ROWNAME = ROWNAME.vec,
+                      VALUE = VALUE.vec,
+                      ZSCORE = rep(NA, length(PATIENTID.vec)),
+                      TYPE = TYPE.vec
+                      ##SUBSET = as.integer(SUBSET.vec), stringsAsFactors=FALSE
+                      )
+
+  # z-score computation must be executed on both cohorts, hence it happens after all the data are in res.df
+  rownames <- unique(res.df$ROWNAME)
+  for (rowname in rownames) {
+      sub.res.df <- res.df[res.df$ROWNAME == rowname, ]
+      if (sub.res.df[1,]$TYPE == "numeric") {
+          values <- as.numeric(sub.res.df$VALUE)
+          ZSCORE.values <- (values - mean(values)) / sd(values)
+          res.df[res.df$ROWNAME == rowname, ]$ZSCORE <- ZSCORE.values
+      }
+  }
+
+  return(res.df)
+}
+
+
 main <- function(max_rows = 100, sorting = "nodes", ranking = "coef", selections = list(), geneCardsAllowed = FALSE) {
     print(0)
     max_rows <- as.numeric(max_rows)
@@ -65,17 +136,63 @@ main <- function(max_rows = 100, sorting = "nodes", ranking = "coef", selections
     ## hd.df = data.list$HD
     ##ld.list = data.list$LD    
     print(4)
-    saveRDS(ld.list, file="ldList.rds")
+    muh <- buildLowDim(ld.list)
+    print(777)
+    saveRDS(muh, file="quak.rds")
     print(5)
-    if (sorting == "nodes") {
-
-    } else {
-        colNames <- colnames(ld.list[, -c(1,2)])
-        subjects <- as.numeric(sub("_.+", "", colNames))
-        subsets <- as.numeric(substring(colNames, first=nchar(colNames), last=nchar(colNames)))
-        ordering <- order(as.numeric(paste(subjects, subsets, sep="")))
-        ld.list <- cbind(ld.list[, c(1,2)], ld.list[, -c(1,2)][, ordering])
-    }
+    
+    ## We need to transform the LD-Data to match our needs.
+    ## Column 1: Row.Label (unique identifier)
+    ## Column 2: Categoric.Marker 1
+    ## Column 3-n: all Entries from Categoric Marker 2
+    ## All cells in the columns of 3-n:
+    ## Numeric Value for cross-over between categoric-marker 1 of row x and categoric-marker 2 of row y.
+    ## Numeric Value can be: AVG(numeric_value), MEAN(numeric_value), PAT_NUMBER()
+    
+    categoricLists <- grep("categoric", names(ld.list), value=TRUE)
+    
+    lowDataDf <- data.frame(matrix(NA, ncol=1, nrow=length(ld.list[[categoricLists[1]]]$Row.Label)))
+    colnames(lowDataDf) <- c("Row.Label")
+    
+    lowDataDf$Row.Label <- ld.list[[categoricLists[1]]]$Row.Label
+    
+    ## FIRST CATEGORY
+    categoryName <- colnames(ld.list[[categoricLists[1]]])[2] 
+    firstCategory <- substring(categoryName, 0, regexpr("\\.[^\\.]*\\.[^\\.]*$", categoryName)[1])
+    
+    i <- 1
+    firstCategoryItems <- vector('character')
+	for (list in ld.list) {
+    	if (grepl(firstCategory, names(list[2]))[1]) {
+    	    firstCategoryItems[[length(tmpList)+1]] = names(list[2])
+    	    i <- i+1
+  	  }	
+	}
+	
+	## SECOND CATEGORY
+	## We cannot use regex here: Race.Afro.American instead of Race.AfroAmerican ....
+    categoryName <- colnames(ld.list[[categoricLists[i]]])[2] 
+    secondCategory <- substring(categoryName, 0, regexpr("\\.[^\\.]*\\.[^\\.]*$", categoryName)[1])
+    
+    secondCategoryItems <- vector('character')
+	for (list in ld.list) {
+    	if (grepl(secondCategory, names(list[2]))[1]) {
+    	    secondCategoryItems[[length(tmpList)+1]] = names(list[2])
+  	  }	
+	}
+	
+	
+    
+    ## For the time being we only allow the nodes sorting method
+    ## if (sorting == "nodes") {
+	##
+ 	##   } else {
+ 	##      colNames <- colnames(ld.list[, -c(1,2)])
+  	##     subjects <- as.numeric(sub("_.+", "", colNames))
+    ##    subsets <- as.numeric(substring(colNames, first=nchar(colNames), last=nchar(colNames)))
+    ##   ordering <- order(as.numeric(paste(subjects, subsets, sep="")))
+    ##  ld.list <- cbind(ld.list[, c(1,2)], ld.list[, -c(1,2)][, ordering])
+    ##}
     
     write.table(
         ld.list,
